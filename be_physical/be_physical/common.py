9d15c27177ead45439101e925391d81f460c1306
lpardey <lpardey@outlook.com>
2024-02-14 22:00:44 +0000

    remove unnecessary function

--- be_physical/be_physical/common.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-from typing import TypeVar
-
-T = TypeVar("T")
-
-
-def get_choices(enumeration: type[Enum], _: type[T]) -> list[tuple[T, str]]:
-    return [(e.value, e.name.replace("_", " ").title()) for e in enumeration]
