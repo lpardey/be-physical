1c3155fb3d940b866f9d899194fe3e91d23d0469
lpardey <lpardey@outlook.com>
2024-02-15 22:23:56 +0000

    register models in user info admin

--- be_physical/user_info/admin.py
+++ be_physical/user_info/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import UserAnnotation, UserInfo, UserTrackingPoint
+
 # Register your models here.
+admin.site.register([UserInfo, UserAnnotation, UserTrackingPoint])
