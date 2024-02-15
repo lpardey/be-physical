59aa4bf888b85c24bb58098334359b41770a6305
lpardey <lpardey@outlook.com>
2024-02-15 21:37:43 +0000

    add bmi module

---
+++ be_physical/user_info/bmi.py
@@ -0,0 +1,35 @@
+import math
+from enum import StrEnum
+
+
+class BMICategory(StrEnum):
+    """
+    Based on the World Health Organization (WHO) guidelines
+    """
+
+    UNDERWEIGHT = "underweight"
+    HEALTHY_WEIGHT = "healthy weight"
+    OVERWEIGHT = "overweight"
+    OBESITY_CLASS_I = "obesity (class I)"
+    OBESITY_CLASS_II = "obesity (class II)"
+    OBESITY_CLASS_III = "obesity (class III)"
+
+
+BMI_CATEGORY_BOUNDS = {
+    BMICategory.UNDERWEIGHT: 18.5,
+    BMICategory.HEALTHY_WEIGHT: 25.0,
+    BMICategory.OVERWEIGHT: 30.0,
+    BMICategory.OBESITY_CLASS_I: 35.0,
+    BMICategory.OBESITY_CLASS_II: 40.0,
+    BMICategory.OBESITY_CLASS_III: math.inf,
+}
+
+
+def get_category_name(bmi: float) -> str:
+    for category, bound in BMI_CATEGORY_BOUNDS.items():
+        if bmi < bound:
+            return category.value
+
+
+def get_bmi(height: float, weight: float) -> float:
+    return weight / (height**2)
