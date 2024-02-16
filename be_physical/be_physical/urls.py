e19b5d81611545e387feed0d07308a3fdcad6a2d
lpardey <lpardey@outlook.com>
2024-02-16 22:15:14 +0000

    add admin site title, header and index title

--- be_physical/be_physical/urls.py
+++ be_physical/be_physical/urls.py
@@ -18,5 +18,9 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+admin.site.site_title = "Be-Physical Site Admin (DEV)"
+admin.site.site_header = "Be-Physical Administration"
+admin.site.index_title = "Site Administration"
