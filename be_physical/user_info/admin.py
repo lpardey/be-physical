34b8b47aff4bd338be0e9870c357e96c84c23c31
lpardey <lpardey@outlook.com>
2024-02-16 22:15:54 +0000

    add admin class for user info, user annotation and user tracking point

--- be_physical/user_info/admin.py
+++ be_physical/user_info/admin.py
@@ -3,4 +3,26 @@ from django.contrib import admin
 from .models import UserAnnotation, UserInfo, UserTrackingPoint
 
 # Register your models here.
-admin.site.register([UserInfo, UserAnnotation, UserTrackingPoint])
+# admin.site.register([UserAnnotation, UserTrackingPoint])
+
+
+@admin.register(UserInfo)
+class UserInfoAdmin(admin.ModelAdmin):
+    # search_fields = ["user_id"] TODO: Check how this works
+    list_display = ["user_id", "height", "birth_date"]
+    list_filter = ["user_id", "height", "birth_date"]
+    search_fields = ["user_id"]
+
+
+@admin.register(UserAnnotation)
+class UserAnnotationAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "text", "annotation_type", "scope", "status"]
+    list_filter = ["user_id", "annotation_type", "scope", "status"]
+    search_fields = ["user_id", "text"]
+
+
+@admin.register(UserTrackingPoint)
+class UserTrackingPointAdmin(admin.ModelAdmin):
+    list_display = ["user_id", "label", "date", "value"]
+    list_filter = ["user_id", "label", "date"]
+    search_fields = ["user_id", "label"]
