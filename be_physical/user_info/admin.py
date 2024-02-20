9043db1f463830a80d525efdf5be0a3a4af74cb1
lpardey <lpardey@outlook.com>
2024-02-20 20:54:59 +0000

    add UserAnnotationInline and UserTrackingPointInline tabular inline to userinfoadmin class add search field to userinfoadmin

--- be_physical/user_info/admin.py
+++ be_physical/user_info/admin.py
@@ -3,15 +3,25 @@ from django.contrib import admin
 from .models import UserAnnotation, UserInfo, UserTrackingPoint
 
 # Register your models here.
-# admin.site.register([UserAnnotation, UserTrackingPoint])
+
+
+class UserAnnotationInline(admin.TabularInline):
+    model = UserAnnotation
+    extra = 0
+
+
+class UserTrackingPointInline(admin.TabularInline):
+    model = UserTrackingPoint
+    extra = 0
+    ordering = ["label", "-date"]
 
 
 @admin.register(UserInfo)
 class UserInfoAdmin(admin.ModelAdmin):
-    # search_fields = ["user_id"] TODO: Check how this works
+    search_fields = ["user_id__username"]
+    inlines = [UserAnnotationInline, UserTrackingPointInline]
     list_display = ["user_id", "height", "birth_date"]
     list_filter = ["user_id", "height", "birth_date"]
-    search_fields = ["user_id"]
 
 
 @admin.register(UserAnnotation)
