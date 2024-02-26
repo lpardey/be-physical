e6c0566154a6e07fe24bcd26903c19df929a937d
lpardey <lpardey@outlook.com>
2024-02-26 22:07:38 +0000

    change physical_biometrics endpoint path to biometrics add get_tracking _points endpoint

--- be_physical/user_info/urls.py
+++ be_physical/user_info/urls.py
@@ -6,6 +6,11 @@ app_name = "user_info"
 urlpatterns = [
     path("", views.index, name="index"),
     path("data/", views.get_data, name="user_info_data"),
-    path("physical_biometrics/", views.get_physical_biometrics, name="physical_biometrics"),
+    path("biometrics/", views.get_biometrics, name="biometrics"),
+    path("tracking_points", views.get_tracking_points, name="tracking_points"),
     path("create/", views.create, name="create"),
 ]
+
+# GET /users/{id}: Retrieve user information by user ID.
+# GET /users/{id}/biometrics: Retrieve physical biometrics data for a user.
+# POST /users: Create a new user in the database.
