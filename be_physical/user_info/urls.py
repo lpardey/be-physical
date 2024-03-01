e45f1fc4e86ea1d93bbed86e8b0e7b0dbc207f80
lpardey <lpardey@outlook.com>
2024-03-01 22:48:37 +0000

    add update path name parameters

--- be_physical/user_info/urls.py
+++ be_physical/user_info/urls.py
@@ -3,13 +3,19 @@ from django.urls import path
 from . import views
 
 app_name = "user_info"
+
+GET_DATA_VIEW_NAME = "user_info_data"
+GET_BIOMETRICS_VIEW_NAME = "user_info_biometrics"
+GET_TRACKING_POINTS_VIEW_NAME = "user_info_tracking_points"
+GET_ANNOTATIONS_VIEW_NAME = "user_info_annotations"
+CREATE_VIEW_NAME = "user_info_create"
+
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("data/", views.get_data, name="user_info_data"),
-    path("biometrics/", views.get_biometrics, name="biometrics"),
-    path("tracking_points/", views.get_tracking_points, name="tracking_points"),
-    path("annotations/", views.get_annotations, name="annotations"),
-    path("create/", views.create, name="create"),
+    path("data/", views.get_data, name=GET_DATA_VIEW_NAME),
+    path("biometrics/", views.get_biometrics, name=GET_BIOMETRICS_VIEW_NAME),
+    path("tracking_points/", views.get_tracking_points, name=GET_TRACKING_POINTS_VIEW_NAME),
+    path("annotations/", views.get_annotations, name=GET_ANNOTATIONS_VIEW_NAME),
+    path("create/", views.create, name=CREATE_VIEW_NAME),
 ]
 
 # GET /users/{id}: Retrieve user information by user ID.
