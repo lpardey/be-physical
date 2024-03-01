d89ebcf213fbf55f002342af4b6cf0e3f304d166
lpardey <lpardey@outlook.com>
2024-03-01 22:45:49 +0000

    update name parameter values

--- be_physical/authentication/urls.py
+++ be_physical/authentication/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = "authentication"
+
 urlpatterns = [
-    path("login/", views.login, name="login"),
-    path("logout/", views.logout, name="login"),
+    path("login/", views.login, name="auth_login"),
+    path("logout/", views.logout, name="auth_logout"),
 ]
