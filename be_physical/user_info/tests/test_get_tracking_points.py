5f82c95c8d036ad1cd073b95f4bd6d5e1f8ceb8e
lpardey <lpardey@outlook.com>
2024-03-05 16:59:50 +0000

    add test_get_tracking_points

---
+++ be_physical/user_info/tests/test_get_tracking_points.py
@@ -0,0 +1,31 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.test import APIClient
+
+from ..models import UserInfo
+from ..serializers import TrackingPointsSerializer
+from ..urls import GET_TRACKING_POINTS_VIEW_NAME, app_name
+
+
+@pytest.mark.django_db
+def test_get_tracking_points(user_info: UserInfo, api_client_authenticated: APIClient) -> None:
+    expected_response = TrackingPointsSerializer(user_info).data
+
+    url = reverse(f"{app_name}:{GET_TRACKING_POINTS_VIEW_NAME}")
+    response: Response = api_client_authenticated.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == expected_response
+
+
+@pytest.mark.django_db
+def test_get_tracking_points_no_auth(api_client: APIClient) -> None:
+    expected_response = {"detail": "Authentication credentials were not provided."}
+
+    url = reverse(f"{app_name}:{GET_TRACKING_POINTS_VIEW_NAME}")
+    response: Response = api_client.get(url)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == expected_response
