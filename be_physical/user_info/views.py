d9e6eae5d3881ae1ac4986e3b54f7e7559d9a014
lpardey <lpardey@outlook.com>
2024-03-08 23:29:49 +0000

    add get grouped tracking points view

--- be_physical/user_info/views.py
+++ be_physical/user_info/views.py
@@ -1,7 +1,8 @@
+import itertools
+
 from django.contrib.auth.models import User
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, QueryDict
 from django.shortcuts import get_object_or_404
-from django.views.decorators.http import require_http_methods
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
@@ -9,19 +10,15 @@ from rest_framework.request import Request
 
 from .models import UserInfo
 from .serializers import (
-    AnnotationsData,
-    AnnotationSerializer,
+    # AnnotationSerializer,
     BiometricsSerializer,
+    # GroupedTrackingPointSerializer,
     TrackingPointsSerializer,
     UserInfoSerializer,
+    serialize_grouped_tracking_groups,
 )
 
-# Create your views here.
-
-
-@api_view(["GET"])
-def index(request: Request) -> JsonResponse:
-    return JsonResponse("Hello, world. You're at the user_info index.")
+LABELS_QUERY_PARAM = "labels"
 
 
 @api_view(["POST"])
@@ -56,36 +53,74 @@ def get_biometrics(request: Request) -> JsonResponse:
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def get_tracking_points(request: Request) -> JsonResponse:
+    """
+    labels: list, starting_date: datetime, end_date: datetime
+    {
+        "tracking_ponts": [
+            {"label": "push ups", "date": "2023-10-25", value: 6},
+            {"label": "weight", "date": "2023-10-25", "value": 78.5},
+        ]
+    }
+    """
     user_info = get_object_or_404(UserInfo, user=request.user)
     serializer = TrackingPointsSerializer(user_info)
     response = JsonResponse(serializer.data)
     return response
 
 
-def get_tracking_points_by_filter(request: HttpRequest) -> JsonResponse:
-    ...
-
-
-@require_http_methods(["GET"])
-def get_annotations(request: HttpRequest) -> JsonResponse:
-    user_info = get_object_or_404(UserInfo, user_id=request.GET["id"])
-    annotations = [AnnotationSerializer(**annotation) for annotation in user_info.annotations.values()]
-    data = AnnotationsData(annotations=annotations)
-    response = JsonResponse(data.model_dump())
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_grouped_tracking_points(request: Request) -> JsonResponse:
+    """
+    {
+        "tracking_ponts": [
+            {
+                "label": "push ups",
+                "values": [
+                    {"date": "2023-10-23 (datetime)", "value": 5},
+                    {"date": "2023-10-24", "value": 5},
+                ],
+            }
+            {
+                "label": "running",
+                "values": [
+                    {"date": "2024-10-23 (datetime)", "value": 5},
+                    {"date": "2024-10-24", "value": 5},
+                ],
+            }
+    ]
+    }
+    """
+    user_info = get_object_or_404(UserInfo, user=request.user)
+    query_params: QueryDict = request.query_params
+    selected_labels = query_params.getlist(LABELS_QUERY_PARAM)
+    points = user_info.tracking_points.filter(label__label__in=selected_labels).order_by("label__label", "date")
+    points_groups = itertools.groupby(points, key=lambda p: p.label.label)
+    data_serialized = serialize_grouped_tracking_groups(points_groups)
+    # serializer = GroupedTrackingPointsSerializer(points_groups)
+    response = JsonResponse(data_serialized)
     return response
 
 
-def get_annotations_by_filter(request: HttpRequest) -> JsonResponse:
-    ...
+def get_annotations_by_filter(request: HttpRequest) -> None:
+    return
 
 
-def create_tracking_point(request: HttpRequest) -> JsonResponse:
-    ...
+def create_tracking_point(request: HttpRequest) -> None:
+    return
 
 
-def create_annotation(request: HttpRequest) -> JsonResponse:
-    ...
+def create_annotation(request: HttpRequest) -> None:
+    return
 
 
-def get_streak(request: HttpRequest) -> JsonResponse:
-    ...
+def get_streak(request: HttpRequest) -> None:
+    return
+
+
+def create_label() -> None:
+    return
+
+
+def get_labels() -> None:
+    return
