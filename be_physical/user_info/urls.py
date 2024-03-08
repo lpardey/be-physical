7f5492d48ce650fec31a710ed5db1094522e0eb1
lpardey <lpardey@outlook.com>
2024-03-08 23:23:27 +0000

    add grouping tracking points url view

--- be_physical/user_info/urls.py
+++ be_physical/user_info/urls.py
@@ -7,6 +7,7 @@ app_name = "user_info"
 GET_DATA_VIEW_NAME = "user_info_data"
 GET_BIOMETRICS_VIEW_NAME = "user_info_biometrics"
 GET_TRACKING_POINTS_VIEW_NAME = "user_info_tracking_points"
+GET_GROUPED_TRACKING_POINTS_VIEW_NAME = "user_info_grouped_tracking_points"
 GET_ANNOTATIONS_VIEW_NAME = "user_info_annotations"
 CREATE_VIEW_NAME = "user_info_create"
 
@@ -14,10 +15,10 @@ urlpatterns = [
     path("data/", views.get_data, name=GET_DATA_VIEW_NAME),
     path("biometrics/", views.get_biometrics, name=GET_BIOMETRICS_VIEW_NAME),
     path("tracking_points/", views.get_tracking_points, name=GET_TRACKING_POINTS_VIEW_NAME),
-    path("annotations/", views.get_annotations, name=GET_ANNOTATIONS_VIEW_NAME),
+    path(
+        "grouped_tracking_points/",
+        views.get_grouped_tracking_points,
+        name=GET_GROUPED_TRACKING_POINTS_VIEW_NAME,
+    ),
     path("create/", views.create, name=CREATE_VIEW_NAME),
 ]
-
-# GET /users/{id}: Retrieve user information by user ID.
-# GET /users/{id}/biometrics: Retrieve physical biometrics data for a user.
-# POST /users: Create a new user in the database.
