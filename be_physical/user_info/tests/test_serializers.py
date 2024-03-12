326e0e2224854aa130fc83556d04c27eb927a104
lpardey <lpardey@outlook.com>
2024-03-12 14:20:35 +0000

    add test_tracking_points_serializer and test_annotations_serializer

--- be_physical/user_info/tests/test_serializers.py
+++ be_physical/user_info/tests/test_serializers.py
@@ -4,9 +4,11 @@ import pytest
 
 from ..models import UserInfo, UserTrackingPoint
 from ..serializers import (
+    AnnotationsSerializer,
     BiometricsSerializer,
     GroupedTrackingPointSerializer,
     SimplifiedTrackingPointSerializer,
+    TrackingPointsSerializer,
     UserInfoSerializer,
 )
 
@@ -39,6 +41,18 @@ def test_biometrics_serializer(user_info_with_biometrics: UserInfo):
 
 
 @pytest.mark.django_db
+def test_tracking_points_serializer(user_info_with_many_tracking_points: UserInfo):
+    data = TrackingPointsSerializer(user_info_with_many_tracking_points).data
+    expected_data = {
+        "tracking_points": [
+            {"label": point.label.label, "date": str(point.date), "value": point.value}
+            for point in user_info_with_many_tracking_points.tracking_points.all()
+        ]
+    }
+    assert data == expected_data
+
+
+@pytest.mark.django_db
 def test_simplified_tracking_points_serializer(user_tracking_point: UserTrackingPoint):
     data = SimplifiedTrackingPointSerializer(user_tracking_point).data
     expected_data = {"date": str(datetime.date.today()), "value": 5.0}
@@ -50,4 +64,21 @@ def test_grouped_tracking_points_serializer(user_tracking_point: UserTrackingPoi
     input_data = ("test_label", [user_tracking_point] * 2)
     data = GroupedTrackingPointSerializer(input_data).data
     expected_data = {"label": "test_label", "values": [{"date": str(datetime.date.today()), "value": 5.0}] * 2}
+    assert data == expected_data
+
+
+@pytest.mark.django_db
+def test_annotations_serializer(user_info_with_many_annotations: UserInfo):
+    data = AnnotationsSerializer(user_info_with_many_annotations).data
+    expected_data = {
+        "annotations": [
+            {
+                "text": annotation.text,
+                "annotation_type": annotation.annotation_type,
+                "scope": annotation.scope,
+                "status": annotation.status,
+            }
+            for annotation in user_info_with_many_annotations.annotations.all()
+        ]
+    }
     assert data == expected_data
