e68adb46c8c362976c24da740b9f5bb34bcd28ea
lpardey <lpardey@outlook.com>
2024-03-13 19:41:14 +0000

    add parametrized cases for test_create_tracking_point add test_create_tracking_point_no_auth

--- be_physical/user_info/tests/test_create_tracking_point.py
+++ be_physical/user_info/tests/test_create_tracking_point.py
@@ -1,5 +1,3 @@
-import datetime
-
 import pytest
 from django.urls import reverse
 from rest_framework import status
@@ -7,27 +5,53 @@ from rest_framework.response import Response
 
 from ..models import UserInfo, UserTrackingLabel
 from ..urls import CREATE_TRACKING_POINT_VIEW_NAME, app_name
-from .conftest import APIClient
+from .conftest import APIClient, TrackingPointPayload
 
 
 @pytest.mark.django_db
+@pytest.mark.parametrize(
+    "test_payload, expected_status",
+    [
+        pytest.param(TrackingPointPayload(), status.HTTP_201_CREATED, id="Valid data"),
+        pytest.param(
+            TrackingPointPayload(user_info_exists=False), status.HTTP_400_BAD_REQUEST, id="Invalid user info"
+        ),
+        pytest.param(TrackingPointPayload(label_exists=False), status.HTTP_400_BAD_REQUEST, id="Invalid label"),
+        pytest.param(TrackingPointPayload(date="invalid_date"), status.HTTP_400_BAD_REQUEST, id="Invalid date"),
+        pytest.param(TrackingPointPayload(value="invalid_value"), status.HTTP_400_BAD_REQUEST, id="Invalid value"),
+    ],
+)
 def test_create_tracking_point(
+    test_payload: TrackingPointPayload,
+    expected_status: status,
     basic_user_info: UserInfo,
     user_tracking_label: UserTrackingLabel,
     api_client_authenticated: APIClient,
 ):
-    payload = {
-        "user_info": basic_user_info.pk,
-        "label": user_tracking_label.pk,
-        "date": str(datetime.date.today()),
-        "value": 10.0,
-    }
+    payload = test_payload.get_payload(basic_user_info, user_tracking_label)
 
     url = reverse(f"{app_name}:{CREATE_TRACKING_POINT_VIEW_NAME}")
     response: Response = api_client_authenticated.post(url, payload)
 
-    assert response.status_code == status.HTTP_201_CREATED
+    assert response.status_code == expected_status
 
-    payload["id"] = basic_user_info.pk
+    if expected_status == status.HTTP_201_CREATED:
+        payload["id"] = basic_user_info.user.pk
+        expected_response = dict(data=payload)
+        assert response.json() == expected_response
 
-    assert response.json() == payload
+
+@pytest.mark.django_db
+def test_create_tracking_point_no_auth(
+    basic_user_info: UserInfo,
+    user_tracking_label: UserTrackingLabel,
+    api_client: APIClient,
+):
+    payload = TrackingPointPayload().get_payload(basic_user_info, user_tracking_label)
+    expected_response = {"detail": "Authentication credentials were not provided."}
+
+    url = reverse(f"{app_name}:{CREATE_TRACKING_POINT_VIEW_NAME}")
+    response: Response = api_client.get(url, payload)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == expected_response
