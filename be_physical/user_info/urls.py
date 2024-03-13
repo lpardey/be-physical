7f4d7a22df17b21756b8746d3634d6c343ce821b
lpardey <lpardey@outlook.com>
2024-03-13 15:42:39 +0000

    add create tracking point path

--- be_physical/user_info/urls.py
+++ be_physical/user_info/urls.py
@@ -10,11 +10,13 @@ GET_TRACKING_POINTS_VIEW_NAME = "user_info_tracking_points"
 GET_GROUPED_TRACKING_POINTS_VIEW_NAME = "user_info_grouped_tracking_points"
 GET_ANNOTATIONS_VIEW_NAME = "user_info_annotations"
 CREATE_VIEW_NAME = "user_info_create"
+CREATE_TRACKING_POINT_VIEW_NAME = "user_info_create_tracking_point"
 
 urlpatterns = [
     path("data/", views.get_data, name=GET_DATA_VIEW_NAME),
     path("biometrics/", views.get_biometrics, name=GET_BIOMETRICS_VIEW_NAME),
     path("tracking_points/", views.get_tracking_points, name=GET_TRACKING_POINTS_VIEW_NAME),
+    path("tracking_points/create/", views.create_tracking_point, name=CREATE_TRACKING_POINT_VIEW_NAME),
     path(
         "grouped_tracking_points/",
         views.get_grouped_tracking_points,
