d4b8649fe291d20848a538878df731d3d74f9d27
lpardey <lpardey@outlook.com>
2024-03-20 17:45:11 +0000

    add test create annotation

---
+++ be_physical/user_info/tests/test_create_annotation.py
@@ -0,0 +1,54 @@
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.response import Response
+
+from ..models import UserInfo
+from ..urls import CREATE_ANNOTATION_VIEW_NAME, app_name
+from .conftest import AnnotationPayload, APIClient
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "test_payload, expected_status",
+    [
+        pytest.param(AnnotationPayload(), status.HTTP_201_CREATED, id="Valid data"),
+        pytest.param(AnnotationPayload(user_info_exists=False), status.HTTP_400_BAD_REQUEST, id="Invalid user info"),
+        pytest.param(
+            AnnotationPayload(annotation_type="invalid_annotation_type"),
+            status.HTTP_400_BAD_REQUEST,
+            id="invalid_annotation_type",
+        ),
+        pytest.param(AnnotationPayload(scope="invalid_scope"), status.HTTP_400_BAD_REQUEST, id="invalid_scope"),
+        pytest.param(AnnotationPayload(status="invalid_status"), status.HTTP_400_BAD_REQUEST, id="invalid_status"),
+    ],
+)
+def test_create_annotation(
+    test_payload: AnnotationPayload,
+    expected_status: status,
+    basic_user_info: UserInfo,
+    api_client_authenticated: APIClient,
+):
+    payload = test_payload.generate_payload(basic_user_info)
+
+    url = reverse(f"{app_name}:{CREATE_ANNOTATION_VIEW_NAME}")
+    response: Response = api_client_authenticated.post(url, payload)
+
+    assert response.status_code == expected_status
+
+    if expected_status == status.HTTP_201_CREATED:
+        expected_payload = {**payload, "id": basic_user_info.user.pk}
+        expected_response = {"data": expected_payload}
+        assert response.json() == expected_response
+
+
+@pytest.mark.django_db
+def test_create_annotation_no_auth(basic_user_info: UserInfo, api_client: APIClient):
+    payload = AnnotationPayload().generate_payload(basic_user_info)
+    expected_response = {"detail": "Authentication credentials were not provided."}
+
+    url = reverse(f"{app_name}:{CREATE_ANNOTATION_VIEW_NAME}")
+    response: Response = api_client.get(url, payload)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert response.json() == expected_response
