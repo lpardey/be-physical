7ea63d988537107a090fe7cd6153f937fbde14f4
lpardey <lpardey@outlook.com>
2024-03-27 15:28:25 +0000

    refactor

--- be_physical/be_physical/urls.py
+++ be_physical/be_physical/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("user_info/", include("user_info.urls")),
     path("auth/", include("authentication.urls")),
+    path("user_info/", include("user_info.urls")),
 ]
 
 admin.site.site_title = "Be-Physical Site Admin (DEV)"
