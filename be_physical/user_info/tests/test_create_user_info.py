48ae9193f6cbbe203cfcc447f51085306c94dbb7
lpardey <lpardey@outlook.com>
2024-03-28 22:43:36 +0000

    refactor test create user info

--- be_physical/user_info/tests/test_create_user_info.py
+++ be_physical/user_info/tests/test_create_user_info.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pytest
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -6,46 +8,119 @@ from rest_framework.response import Response
 
 from ..urls import CREATE_USER_INFO_VIEW_NAME, app_name
 from .conftest import APIClient
-from .payload import CreateUserInfoPayload
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize(
-    "test_payload, expected_status",
+    "users, height, birth_date, client, expected_status",
     [
-        pytest.param(CreateUserInfoPayload(), status.HTTP_201_CREATED, id="Valid data"),
-        pytest.param(CreateUserInfoPayload(user_exists=False), status.HTTP_400_BAD_REQUEST, id="Invalid user"),
-        pytest.param(CreateUserInfoPayload(height=""), status.HTTP_400_BAD_REQUEST, id="Missing height field"),
-        pytest.param(CreateUserInfoPayload(height="invalid_height"), status.HTTP_400_BAD_REQUEST, id="Invalid height"),
-        pytest.param(CreateUserInfoPayload(birth_date="invalid_date"), status.HTTP_400_BAD_REQUEST, id="Invalid date"),
-        pytest.param(CreateUserInfoPayload(birth_date=""), status.HTTP_400_BAD_REQUEST, id="Missing date field"),
+        pytest.param(
+            "user",
+            "2.00",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_201_CREATED,
+            id="Valid request regular user",
+        ),
+        pytest.param(
+            "admin_user",
+            "2.00",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_201_CREATED,
+            id="Valid request admin user",
+        ),
+        pytest.param(
+            "user",
+            "invalid_height",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Invalid height",
+        ),
+        pytest.param(
+            "user",
+            "5.4563",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Height with more than two decimal values",
+        ),
+        pytest.param(
+            "user",
+            "5.00",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Height value out of upper limit",
+        ),
+        pytest.param(
+            "user",
+            "0.99",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Height value out of lower limit",
+        ),
+        pytest.param(
+            "user",
+            "",
+            "1989-09-01",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Missing height",
+        ),
+        pytest.param(
+            "user",
+            "2.00",
+            "invalid_date",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Invalid date",
+        ),
+        pytest.param(
+            "user",
+            "2.00",
+            datetime.date.today() + datetime.timedelta(days=1),
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Birth date out of upper limit",
+        ),
+        pytest.param(
+            "user",
+            "2.00",
+            "1899-01-01",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Birth date out of lower limit",
+        ),
+        pytest.param(
+            "user",
+            "2.00",
+            "",
+            "api_client_authenticated",
+            status.HTTP_400_BAD_REQUEST,
+            id="Missing date",
+        ),
+        pytest.param(
+            "user",
+            "2.00",
+            "1989-09-01",
+            "api_client",
+            status.HTTP_401_UNAUTHORIZED,
+            id="Unauthorized user",
+        ),
     ],
+    indirect=["users", "client"],
 )
-def test_create_user_info(
-    test_payload: CreateUserInfoPayload,
-    expected_status: status,
-    user: User,
-    api_client_authenticated: APIClient,
-):
-    payload = test_payload.generate_payload(user)
+def test_create_user_info(users: User, height: float, birth_date: str, client: APIClient, expected_status: status):
+    data = {"user": users.pk, "height": height, "birth_date": birth_date}
 
     url = reverse(f"{app_name}:{CREATE_USER_INFO_VIEW_NAME}")
-    response: Response = api_client_authenticated.post(url, payload)
+    response: Response = client.post(url, data)
 
     assert response.status_code == expected_status
 
     if response.status_code == status.HTTP_201_CREATED:
-        expected_response = dict(data=payload)
+        expected_response = dict(data=data)
         assert response.json() == expected_response
-
-
-@pytest.mark.django_db
-def test_create_user_info_no_auth(user: User, api_client: APIClient):
-    payload = CreateUserInfoPayload().generate_payload(user)
-    expected_response = {"detail": "Authentication credentials were not provided."}
-
-    url = reverse(f"{app_name}:{CREATE_USER_INFO_VIEW_NAME}")
-    response: Response = api_client.post(url, payload)
-
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == expected_response
