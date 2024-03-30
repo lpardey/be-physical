e8d369be9d9db9e0df1224e07820fb3e54137f29
lpardey <lpardey@outlook.com>
2024-03-30 01:37:48 +0000

    fix birth date validator

--- be_physical/user_info/validators.py
+++ be_physical/user_info/validators.py
@@ -6,10 +6,10 @@ from django.utils.translation import gettext as _
 
 # UserInfo model
 def validate_birth_date(value: datetime.date) -> datetime.date:
-    min_date_limit = datetime.date(1900, 1, 1)
+    min_date_limit = datetime.date.today() - datetime.timedelta(days=36500)
     max_date = datetime.date.today()
 
-    if value > max_date or value < min_date_limit:
+    if not (min_date_limit <= value < max_date):
         raise ValidationError(_("Invalid date"))
 
     return value
