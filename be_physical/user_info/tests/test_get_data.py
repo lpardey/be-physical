2a9c3a7d5c2f16cf7c8fd99efdac91a63d054480
lpardey <lpardey@outlook.com>
2024-04-01 15:17:20 +0000

    refactor test get data

--- be_physical/user_info/tests/test_get_data.py
+++ be_physical/user_info/tests/test_get_data.py
@@ -1,3 +1,4 @@
+from typing import Any
 from unittest.mock import Mock, patch
 
 import pytest
@@ -48,23 +49,32 @@ def test_get_data_for_other_user(m_get_object_or_404: Mock, user: User, api_clie
 
 
 @pytest.mark.django_db
-def test_get_data_unauthorized_user(api_client: APIClient):
-    url = reverse(f"{app_name}:{GET_DATA_VIEW_NAME}")
-
-    response: Response = api_client.get(url)
-
-    assert response.status_code == status.HTTP_401_UNAUTHORIZED
-    assert response.json() == UNAUTHORIZED_RESPONSE
-
-
+@pytest.mark.parametrize(
+    "client_fixture, expected_status, expected_response",
+    [
+        pytest.param(
+            "api_client",
+            status.HTTP_401_UNAUTHORIZED,
+            UNAUTHORIZED_RESPONSE,
+            id="Unauthorized",
+        ),
+        pytest.param(
+            "api_client_authenticated",
+            status.HTTP_404_NOT_FOUND,
+            USER_INFO_NOT_FOUND_RESPONSE,
+            id="UserInfo not found",
+        ),
+    ],
+    indirect=["client_fixture"],
+)
 @pytest.mark.django_db
-def test_get_data_user_info_not_found(api_client_authenticated: APIClient):
+def test_get_data_failed(client_fixture: APIClient, expected_status: status, expected_response: dict[str, Any]):
     url = reverse(f"{app_name}:{GET_DATA_VIEW_NAME}")
 
-    response: Response = api_client_authenticated.get(url)
+    response: Response = client_fixture.get(url)
 
-    assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert response.json() == USER_INFO_NOT_FOUND_RESPONSE
+    assert response.status_code == expected_status
+    assert response.json() == expected_response
 
 
 @pytest.mark.django_db
