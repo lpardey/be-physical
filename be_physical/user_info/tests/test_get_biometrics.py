9b0bc5b34fff8fbfed4a801232417bff0cb674c9
lpardey <lpardey@outlook.com>
2024-04-01 15:29:44 +0000

    refactor

--- be_physical/user_info/tests/test_get_biometrics.py
+++ be_physical/user_info/tests/test_get_biometrics.py
@@ -32,23 +32,6 @@ def test_get_biometrics_success(user_fixture: User, user_info_fixture: UserInfo,
 
 
 @pytest.mark.django_db
-@patch("user_info.views.get_object_or_404")
-def test_get_biometrics_for_other_user(m_get_object_or_404: Mock, user: User, api_client_authenticated: APIClient):
-    assert not user.is_staff
-    assert not user.is_superuser
-
-    target_user = User.objects.create(username="target_user", email="target_user@example.com", password="12345")
-    target_user_info = UserInfo.objects.create(user=target_user, height="1.80", birth_date="1990-01-01")
-    m_get_object_or_404.return_value = target_user_info
-    url = reverse(f"{app_name}:{GET_BIOMETRICS_VIEW_NAME}")
-
-    response: Response = api_client_authenticated.get(url)
-
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert response.json() == PERMISSION_DENIED_RESPONSE
-
-
-@pytest.mark.django_db
 @pytest.mark.parametrize(
     "client_fixture, expected_status, expected_response",
     [
@@ -68,7 +51,7 @@ def test_get_biometrics_for_other_user(m_get_object_or_404: Mock, user: User, ap
     indirect=["client_fixture"],
 )
 @pytest.mark.django_db
-def test_get_data_failed(client_fixture: APIClient, expected_status: status, expected_response: dict[str, Any]):
+def test_get_biometrics_failed(client_fixture: APIClient, expected_status: status, expected_response: dict[str, Any]):
     url = reverse(f"{app_name}:{GET_BIOMETRICS_VIEW_NAME}")
 
     response: Response = client_fixture.get(url)
@@ -78,8 +61,25 @@ def test_get_data_failed(client_fixture: APIClient, expected_status: status, exp
 
 
 @pytest.mark.django_db
+@patch("user_info.views.get_object_or_404")
+def test_get_biometrics_for_other_user(m_get_object_or_404: Mock, user: User, api_client_authenticated: APIClient):
+    assert not user.is_staff
+    assert not user.is_superuser
+
+    target_user = User.objects.create(username="target_user", email="target_user@example.com", password="12345")
+    target_user_info = UserInfo.objects.create(user=target_user, height="1.80", birth_date="1990-01-01")
+    m_get_object_or_404.return_value = target_user_info
+    url = reverse(f"{app_name}:{GET_BIOMETRICS_VIEW_NAME}")
+
+    response: Response = api_client_authenticated.get(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert response.json() == PERMISSION_DENIED_RESPONSE
+
+
+@pytest.mark.django_db
 @pytest.mark.parametrize("http_method", ["post", "put", "patch", "delete"])
-def test_get_data_incorrect_http_method(http_method: str, api_client_authenticated: APIClient):
+def test_get_biometrics_incorrect_http_method(http_method: str, api_client_authenticated: APIClient):
     url = reverse(f"{app_name}:{GET_BIOMETRICS_VIEW_NAME}")
     request_method = getattr(api_client_authenticated, http_method)
 
